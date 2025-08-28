844d46dc44d7f43bdeab5f746abf2498195900e9
matiasssssas <matiasmorillas1412@gmail.com>
2025-08-28 10:13:57 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,75 @@
 def ej1():
-    numeros = [10, 20, 30, 40, 50]
-    suma_total = 0
-    for numero in numeros:
-        suma_total += numero
-    print("la suma total", suma_total)
+   matriz = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+
+
+   for fila in matriz:
+       print(fila)
+
+ej1()
 
 def ej2():
-    cadena = "Programacion python"
-    contador_vocales = 0
-    vocales = "aeiouAEIOU"
-    for caracter in cadena:
-        if caracter in vocales:
-            print("cantidad de vocales:", contador_vocales)
-            
+   matriz = [[10, 20, 30],
+             [40, 50, 60],
+             [70, 80, 90]]
+
+
+   suma_total = 0
+   for fila in matriz:
+       suma_total += sum(fila)
+   print("La suma de todos los números en la matriz es:", suma_total)
+
+
+
+ej2()
+
 
 def ej3():
-    numero = int(input("ingresa un numero entero:"))
-    for i in range(1,11):
-        producto = numero * i
-        print(f"{numero} x {i} = {producto}")
+   matriz = [
+       [1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [9, 10, 11, 12],
+       [13, 14, 15, 16]]
+
+
+
+
+   fila = int(input("Ingresa el índice de la fila 0 a 3: "))
+   columna = int(input("Ingresa el índice de la columna 0 a 3: "))
+
+
+
+
+   if 0 <= fila < 4 and 0 <= columna < 4:
+       elemento = matriz[fila][columna]
+       print(f"El elemento en la posición ({fila}, {columna}) es: {elemento}")
+   else:
+       print("Índice fuera de rango. Por favor ingresa valores entre 0 y 3.")
+
+
+ej3()
+
 
 def ej4():
-    numeros = [1,2,3,4,5,6,7,8,9,10]
-    pares = []
-    for numero in numeros:
-        if numero % 2 == 0:
-            pares.append(numero)
-            print("Numeros pares:", pares)
-            print("cantidad de pares:", len(pares))
+    def encontrar_maximo():
+        matriz = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]]
 
-def ej5():
-    filas = 5
-    for i in range(1, filas + 1):
-        for j in range(i):
-            print("*", end="")
-        print()
+        maximo = matriz[0][0]
+        for fila in matriz:
+            for elemento in fila:
+                if elemento > maximo:
+                    maximo = elemento
+
+        print("La matriz es:")
+        for fila in matriz:
+            print(fila)
+        print(f"\nEl número más grande de la matriz es: {maximo}")
+    encontrar_maximo()
 
 
+ej4()
