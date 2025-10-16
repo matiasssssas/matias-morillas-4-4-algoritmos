c999611cd70106a20a5d34874a5675e94f46d178
matiasssssas <matiasmorillas1412@gmail.com>
2025-10-16 08:59:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,146 @@
-def ej1():
-    numeros = [10, 20, 30, 40, 50]
-    suma_total = 0
-    for numero in numeros:
-        suma_total += numero
-    print("la suma total", suma_total)
-
-def ej2():
-    cadena = "Programacion python"
-    contador_vocales = 0
-    vocales = "aeiouAEIOU"
-    for caracter in cadena:
-        if caracter in vocales:
-            print("cantidad de vocales:", contador_vocales)
-            
-
-def ej3():
-    numero = int(input("ingresa un numero entero:"))
-    for i in range(1,11):
-        producto = numero * i
-        print(f"{numero} x {i} = {producto}")
-
-def ej4():
-    numeros = [1,2,3,4,5,6,7,8,9,10]
-    pares = []
-    for numero in numeros:
-        if numero % 2 == 0:
-            pares.append(numero)
-            print("Numeros pares:", pares)
-            print("cantidad de pares:", len(pares))
-
-def ej5():
-    filas = 5
-    for i in range(1, filas + 1):
-        for j in range(i):
-            print("*", end="")
-        print()
+def ejercicio1():
 
 
+   productos = [
+       {"nombre": "Laptop", "precio": 1200, "categoria": "Electrónica"},
+       {"nombre": "Mouse", "precio": 25, "categoria": "Electrónica"},
+       {"nombre": "Teclado", "precio": 75, "categoria": "Electrónica"},
+       {"nombre": "Silla de Oficina", "precio": 300, "categoria": "Muebles"}
+   ]
+
+
+   for producto in productos:
+       print(producto["nombre"])
+
+
+
+
+def ejercicio2():
+
+
+   productos = [
+
+
+       {"nombre": "Laptop", "precio": 1200, "categoria": "Electrónica"},
+       {"nombre": "Mouse", "precio": 25, "categoria": "Electrónica"},
+       {"nombre": "Teclado", "precio": 75, "categoria": "Electrónica"},
+       {"nombre": "Silla de Oficina", "precio": 300, "categoria": "Muebles"}
+   ]
+
+
+   total = 0
+   for producto in productos:
+       total = total + producto["precio"]
+   print("La suma total es:", total)
+
+
+
+
+def ejercicio3():
+
+
+   productos = [
+
+
+       {"nombre": "Laptop", "precio": 1200, "categoria": "Electrónica"},
+       {"nombre": "Mouse", "precio": 25, "categoria": "Electrónica"},
+       {"nombre": "Teclado", "precio": 75, "categoria": "Electrónica"},
+       {"nombre": "Silla de Oficina", "precio": 300, "categoria": "Muebles"}
+   ]
+   productos.append("valorant")
+   print(productos)
+
+
+
+
+def ejercicio4():
+
+
+   estudiantes = [
+       {"nombre": "Ana", "edad": 21, "calificacion": 90},
+       {"nombre": "Luis", "edad": 22, "calificacion": 95},
+       {"nombre": "Marta", "edad": 20, "calificacion": 85}
+   ]
+
+
+   estudiantes[2]["edad"] = 1000
+   print(estudiantes[2])
+
+
+
+
+def ejercicio5():
+
+
+   estudiantes = [
+       {"nombre": "Ana", "edad": 21, "calificacion": 90},
+       {"nombre": "Luis", "edad": 22, "calificacion": 95},
+       {"nombre": "Marta", "edad": 20, "calificacion": 85}
+   ]
+
+
+   notamejor = estudiantes[0]
+   for estudiante in estudiantes [1:]:
+       if estudiante["calificacion"] > notamejor["calificacion"]:
+           estudiantemejor = estudiante
+       print("estudiante con la mejor calificacion", estudiantemejor)
+
+
+
+
+def ejercicio6():
+
+
+   libros = [
+       {"titulo": "Cien Años de Soledad", "autor": "Gabriel García Márquez"},
+       {"titulo": "Don Quijote", "autor": "Miguel de Cervantes"},
+       {"titulo": "La Sombra del Viento", "autor": "Carlos Ruiz Zafón"}
+   ]
+
+
+   titulos = []
+   for libro in libros:
+       titulos.append(libro["titulo"])
+
+
+   print(titulos)
+
+
+
+
+def ejercicio7():
+
+
+   libros = [
+       {"titulo": "Cien Años de Soledad", "autor": "Gabriel García Márquez"},
+       {"titulo": "Don Quijote", "autor": "Miguel de Cervantes"},
+       {"titulo": "La Sombra del Viento", "autor": "Carlos Ruiz Zafón"}
+   ]
+
+
+   libros = [libro for libro in libros if libro["titulo"] != "Don Quijote"]
+
+
+   libros.append({"titulo": "Don Quijote", "autor": "Miguel de Cervantes"})
+
+
+   print(libros)
+
+
+
+
+def ejercicio8():
+
+
+   libros = [
+       {"titulo": "Cien Años de Soledad", "autor": "Gabriel García Márquez"},
+       {"titulo": "Don Quijote", "autor": "Miguel de Cervantes"},
+       {"titulo": "La Sombra del Viento", "autor": "Carlos Ruiz Zafón"}
+   ]
+
+
+   for libro in libros:
+       libro["disponible"] = True
+
+
+   print(libros)
