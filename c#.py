dc2008fb116fd7e0276765a81087fc47d875804a
matiasssssas <matiasmorillas1412@gmail.com>
2026-06-12 11:41:34 -0300

    Update and rename main.py to c#.py

---
+++ c#.py
@@ -0,0 +1,12 @@
+int[,] MiMatriz = LlenarMatriz(4, 4);
+
+
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write(MiMatriz[i, j] + "");
+    }
+        Console.WriteLine();
+}
